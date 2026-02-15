51315ffa592fb49f4192b4c21f01e0f36b96cfe9
Ybalrid (Arthur Brainville) <ybalrid@ybalrid.info>
2026-02-15 20:18:56 +0100

    Create a class to hopefully manage... Viewports? Will see

---
+++ Sources/KSA.XR/XrViewports.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KSA;
+
+namespace KSA.XR
+{
+	internal class XrViewports
+	{
+		KSA.Camera[] eyeCameras = new KSA.Camera[2];
+
+	}
+}
