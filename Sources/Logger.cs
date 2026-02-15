72031cc6b1773be30f381cf9d287d8842b335fa6
Ybalrid (Arthur Brainville) <ybalrid@ybalrid.info>
2026-02-15 04:13:39 +0100

    Little bit of code cleanup

---
+++ Sources/Logger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KSA
+{
+	namespace XR
+	{
+		internal class Logger
+		{
+			private static void writeTag(string? extra = null)
+			{
+				var bgRestore = Console.BackgroundColor;
+				var fgRestore = Console.ForegroundColor;
+
+				Console.BackgroundColor = ConsoleColor.Black;
+				Console.ForegroundColor = ConsoleColor.Magenta;
+				Console.Write("[KSA_XR");
+				if (extra != null)
+					Console.Write($" {extra}");
+				Console.Write("] ");
+				Console.BackgroundColor = bgRestore;
+				Console.ForegroundColor = fgRestore;
+			}
+
+			public static void warning(string warning, string? tag = null)
+			{
+				writeTag(tag);
+				var bgRestore = Console.BackgroundColor;
+				var fgRestore = Console.ForegroundColor;
+
+				Console.BackgroundColor = ConsoleColor.Black;
+				Console.ForegroundColor = ConsoleColor.Yellow;
+
+				Console.WriteLine(warning);
+
+				Console.BackgroundColor = bgRestore;
+				Console.ForegroundColor = fgRestore;
+			}
+
+			public static void error(string error, string? tag = null)
+			{
+				writeTag(tag);
+				var bgRestore = Console.BackgroundColor;
+				var fgRestore = Console.ForegroundColor;
+
+				Console.BackgroundColor = ConsoleColor.Black;
+				Console.ForegroundColor = ConsoleColor.Red;
+
+				Console.WriteLine(error);
+
+				Console.BackgroundColor = bgRestore;
+				Console.ForegroundColor = fgRestore;
+			}
+
+
+			public static void message(string message, string? tag = null)
+			{
+				writeTag(tag);
+				Console.WriteLine(message);
+			}
+		}
+	}
+}
---
+++ Sources/ModLoader.cs
@@ -0,0 +1,47 @@
+﻿using StarMap.API;
+using HarmonyLib;
+using System.Reflection;
+
+namespace KSA
+{
+	namespace XR
+	{
+		/// <summary>
+		/// The mod's main entry point. This class sereves as the holder of state for the rest of the XR mod
+		/// </summary>
+		[StarMapMod]
+		public class ModLoader
+		{
+			public static OpenXR? openxr;
+			public static DebugUI? ui;
+			public static Harmony harmony = new Harmony("KSA_XR.patches");
+
+
+			[StarMapBeforeMain]
+			public void preMain()
+			{
+				Logger.message("Loading KSA_XR");
+
+				//It is very important to install the patches to KSA before we initialize anything else.
+				harmony.PatchAll(Assembly.GetExecutingAssembly());
+				Logger.message("Harmony patches have been installed.");
+				openxr = new OpenXR();
+				Logger.message("OpenXR 1st stage initialization done!");
+
+				ui = new DebugUI();
+			}
+
+			[StarMapAfterGui]
+			public void UIPulse(double dt)
+			{
+				ui?.StatusWindow();
+			}
+
+			[StarMapAfterOnFrame]
+			public void OnFrame(double dta, double dtb) //Note: I found the function signature in StarMap's source code. I have not checked what these are, but they look close enough to frame timings lol
+			{
+				openxr?.OnFrame(dta);
+			}
+		}
+	}
+}
