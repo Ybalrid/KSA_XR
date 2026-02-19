8a316f0ee62f2918705ed7adaedf987d96f1bd04
Ybalrid (Arthur Brainville) <ybalrid@ybalrid.info>
2026-02-19 23:37:58 +0100

    Remove useless patch code

--- Sources/KSA.XR/XrViewports.cs
+++ Sources/KSA.XR/XrViewports.cs
@@ -88,11 +88,6 @@ namespace KSA.XR
 			m.M43 = (nearPlaneDistance * farPlaneDistance) / (farPlaneDistance - nearPlaneDistance);
 			m.M44 = 0f;
 
-			/*//hack force symetry for debug
-			m.M13 = 0;
-			m.M23 = 0;
-			*/
-
 			return m;
 		}
 
@@ -156,58 +151,5 @@ namespace KSA.XR
 
 	internal class XrViewports
 	{
-		public static unsafe void RenderViewport(Brutal.VulkanApi.CommandBuffer commandBuffer, Viewport viewport, int frameIndex)
-		{
-			var program = KSA.Program.Instance;
-
-			var renderViewport = AccessTools.Method(typeof(KSA.Program), "RenderViewport");
-
-			renderViewport.Invoke(program, new object[]
-			{
-				commandBuffer, viewport, frameIndex
-			});
-
-		}
-
-
-		public static KSA.Viewport? AddViewport(int2 framebufferSize, bool buildRenderTarget, bool centerViewport)
-		{
-			try
-			{
-				/*
-				var program = KSA.Program.Instance;
-				
-				var addViewport = AccessTools.Method(typeof(KSA.Program), "AddViewport");
-				var before = KSA.Program.Viewports.Count;
-
-				addViewport.Invoke(program, new object[]
-				{
-					framebufferSize,
-					buildRenderTarget,
-					centerViewport
-				});
-
-				program.RebuildRenderer();
-
-				return KSA.Program.Viewports[before];
-		
-				*/
-
-				/*
-							Camera camera = new Camera(framebufferSize);
-							camera.SetPosition(-28423595433.0, 350059017438.0, 952938436359.0);
-							camera.LookAt(double3.Zero, Double3Ex.Up);
-							GameSettings.Current.ApplyTo(camera);*/
-
-			}
-			catch (Exception e)
-			{
-				//trap all
-				Logger.error(e.ToString());
-			}
-
-
-			return null;
-		}
 	}
 }
